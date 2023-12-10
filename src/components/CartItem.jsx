8b94854dd80d804220ab766ea9c02763a31211df
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-10 23:07:40 +0530

    added

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { AiOutlinePlus, AiOutlineMinus, AiOutlineDelete } from "react-icons/ai";
 import { useDispatch } from "react-redux";
-import { removeFromCart } from "../redux/slices/CartSlices";
+import {
+  decreaseQty,
+  increaseQty,
+  removeFromCart,
+} from "../redux/slices/CartSlices";
 
 const CartItem = ({ id, img, name, price, qty }) => {
   const dispatch = useDispatch();
+
+  const handleIncreaseQty = () => {
+    dispatch(increaseQty({ id }));
+  };
+
+  const handleDecreaseQty = () => {
+    if (qty > 1) {
+      dispatch(decreaseQty({ id }));
+    } else {
+      dispatch(removeFromCart({ id, img, name, price, qty }));
+    }
+  };
+
   return (
-    <div className="flex gap-2 shadow-lg p-2 mb-3">
+    <div className="flex gap-2 shadow-lg p-2 mb-3 relative">
       <AiOutlineDelete
         onClick={() => dispatch(removeFromCart({ id, img, name, price, qty }))}
         className="absolute right-7 cursor-pointer hover:text-red-600"
@@ -17,9 +34,15 @@ const CartItem = ({ id, img, name, price, qty }) => {
         <div className="flex justify-between">
           <span className="text-green-500 font-bold">₹ {price}</span>
           <div className="flex justify-center items-center gap-2 absolute right-7">
-            <AiOutlinePlus className="border-2  hover:bg-green-500 hover:border-none p-1 text-xl transition-all ease-linear rounded-md cursor-pointer text-red-500 border-gray-600" />
+            <AiOutlinePlus
+              onClick={handleIncreaseQty}
+              className="border-2 hover:bg-green-500 hover:border-none p-1 text-xl transition-all ease-linear rounded-md cursor-pointer text-red-500 border-gray-600"
+            />
             <span>{qty}</span>
-            <AiOutlineMinus className="border-2  hover:bg-green-500 hover:border-none p-1 text-xl transition-all ease-linear rounded-md cursor-pointer text-red-500 border-gray-600" />
+            <AiOutlineMinus
+              onClick={handleDecreaseQty}
+              className="border-2 hover:bg-green-500 hover:border-none p-1 text-xl transition-all ease-linear rounded-md cursor-pointer text-red-500 border-gray-600"
+            />
           </div>
         </div>
       </div>
