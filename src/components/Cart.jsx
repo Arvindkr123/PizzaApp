a968ae07415ea9b85a7b5e4a15eb94084c8d8d7a
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-10 22:19:03 +0530

    added cartitem sidebar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
 import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 const Cart = () => {
+  const [cartActive, setCartActive] = useState(true);
+  const data = useSelector((state) => state.cart.cart);
   return (
     <>
-      <div className="p-5 fixed right-0 top-0 lg:w-[20vw] w-full bg-white h-full">
+      <div
+        className={`p-5 fixed right-0 top-0 lg:w-[20vw] w-full bg-white h-full ${
+          cartActive ? "translate-x-0" : "translate-x-full"
+        } transition-all duration-500 z-50`}
+      >
         <div className="flex justify-between items-center my-3">
           <span className="font-bold text-xl text-gray-800">My Order</span>
           <span>
-            <IoMdClose className="cursor-pointer border-2 p-1 text-xl rounded-md hover:text-red-300 border-gray-600 text-gray-600 font-bold hover:border-red-300" />
+            <IoMdClose
+              onClick={() => setCartActive(!cartActive)}
+              className="cursor-pointer border-2 p-1 text-xl rounded-md hover:text-red-300 border-gray-600 text-gray-600 font-bold hover:border-red-300"
+            />
           </span>
         </div>
         {/* Item card added start */}
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {data.map((ele) => {
+          return <CartItem key={ele.id} {...ele} />;
+        })}
         {/* Item card added  end */}
 
         {/* For the total amount */}
@@ -29,6 +38,10 @@ const Cart = () => {
           </button>
         </div>
       </div>
+      <FaShoppingCart
+        onClick={() => setCartActive(!cartActive)}
+        className="rounded-full bg-white text-5xl shadow-md p-3 fixed bottom-4 right-4 cursor-pointer"
+      />
     </>
   );
 };
--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/slices/CartSlices";
 const FoodCard = ({ id, img, name, price, desc, category, rating }) => {
+  const dispatch = useDispatch();
   return (
     <div className="font-bold w-[250px] bg-white flex flex-col p-5 rounded-lg mx-5 gap-2">
       <img
@@ -12,13 +15,18 @@ const FoodCard = ({ id, img, name, price, desc, category, rating }) => {
         <h2>Onion Pizza</h2>
         <span className="text-green-500">₹{price}</span>
       </div>
-      <p className="text-sm font-normal">{desc.slice(0,50)}...</p>
+      <p className="text-sm font-normal">{desc.slice(0, 50)}...</p>
       <div className="flex justify-between items-center">
         <span className="flex items-center text-sm font-mono justify-center gap-1">
           <AiFillStar className="text-yellow-400" />
           {rating}
         </span>
-        <button className="rounded-lg text-sm p-1 text-white bg-green-500 hover:bg-green-600">
+        <button
+          onClick={() => {
+            dispatch(addToCart({ id, img, name, price, qty: 1 }));
+          }}
+          className="rounded-lg text-sm p-1 text-white bg-green-500 hover:bg-green-600"
+        >
           Add to Cart
         </button>
       </div>
