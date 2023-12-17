35075c1d7c21bd52e18a14d456d1c6f92e132397
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-17 13:16:05 +0530

    applied filter

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FoodCard from "./FoodCard";
 import FoodData from "../utils/FoodData";
+import { useSelector } from "react-redux";
 
 const FoodItem = () => {
+  const category = useSelector((state) => state.category.category);
   return (
     <div className="flex flex-wrap justify-center lg:justify-start mx-6 my-10 gap-10">
-      {FoodData.map((food) => {
+      {FoodData.filter((item) => {
+        if (item.category === "All" || category === "All") {
+          return item;
+        }
+        return item.category === category;
+      }).map((food) => {
         return <FoodCard key={food.id} {...food} />;
       })}
     </div>
